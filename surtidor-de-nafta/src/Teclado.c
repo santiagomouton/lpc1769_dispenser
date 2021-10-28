61b15b46abcb6b39defb18abfa67a957caec1340
RugenKunkka <stefano.itr.unc@gmail.com>
2021-10-28 18:40:26 -0300

    Teclado por EINT

--- surtidor-de-nafta/src/Teclado.c
+++ surtidor-de-nafta/src/Teclado.c
@@ -18,11 +18,12 @@ void configurarPuertosTeclado(){
 	int i;
 	for(i=0 ; i<4; i++){
 		LPC_GPIO2->FIODIR|=(1<<pinesFilas[i]);//pines como output de las filas del teclado
-		LPC_GPIO2->FIOSET|=(1<<pinesFilas[i]);//pines de las filas en alto del teclado
+		LPC_GPIO2->FIOCLR|=(1<<pinesFilas[i]);//pines de las filas en alto del teclado//set en el otro
 	}
 	//el resto de los pines están como input y pull up asi que las columnas del teclado están configuradas ya...
 }
 
+/*Detecta la tecla que fue presionada, la podemos llamar desde el main periódicamete o por interrupciones externas*/
 void loopTeclado(){
 	//Barrido por las filas
 	char teclaPresionada=' ';
@@ -41,4 +42,23 @@ void loopTeclado(){
 	      LPC_GPIO2->FIOSET|=(1<<pinesFilas[nL]);
 	    }
 	   retardoEnMs(10);
+}
+
+/*condiguración para habilitar interrupciones por GPIO para el teclado matricial*/
+void confIntGPIOPorEINT(void){
+	for(int i=0; i<4;i++){
+		LPC_GPIOINT -> IO2IntEnF |= ((1 << pinesColumnas[i])); //Selecciono la interrupcion por flanco de bajada
+		LPC_GPIOINT -> IO2IntClr |= ((1 << pinesColumnas[i])); //Limpia la bandera
+	}
+	NVIC_SetPriority(EINT3_IRQn, 0); 	//Prioridad para esta interrupcion
+	NVIC_EnableIRQ(EINT3_IRQn); 		//Habilita las interrupciones por GPIO
+}
+
+void EINT3_IRQHandler(void){
+	NVIC_DisableIRQ(EINT3_IRQn);
+	for(int i=0; i<4;i++){
+		LPC_GPIOINT -> IO2IntClr |= ((1 << pinesColumnas[i])); //Limpia la bandera
+	}
+	loopTeclado();
+	NVIC_EnableIRQ(EINT3_IRQn);
 }
