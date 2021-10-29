ecae10f601af47e628483b35bcad3bf88e1601f2
RugenKunkka <stefano.itr.unc@gmail.com>
2021-10-29 09:34:01 -0300

    MDE terminada y todo en el main

--- surtidor-de-nafta/src/main.c
+++ surtidor-de-nafta/src/main.c
@@ -14,8 +14,10 @@
 
 #include <cr_section_macros.h>
 #include "Utils.h"
-#include "Teclado.h"
-#include "Uart.h"
+#include <math.h>
+#include <stdlib.h>
+//#include "Teclado.h"
+//#include "Uart.h"
 
 
 void estadosAdmin(void);
@@ -25,10 +27,23 @@ char ingresadoPorTeclado[10]="";
 int precioNafta=110;
 int precioGasoil=90;
 
+int cantidadDeLitrosACargar=0;
+
 char modoCombustible='0';//0-Espera orden, 1-Carga Nafta, 2-Carga Gasoil
 char modoCarga='0';//0-Espera orden, 1-Cant de litros, 2-Hasta que llene, 3-Modo abierto
+char modoIngresarCantidad='0';//1-cantidad de litros, //2-cantidad de plata
 char estadoDispenser='0';//1-llenando, 2-Esperando evento de llenado
 
+
+/*#########Variables del teclado#########*/
+int pinesFilas[] = {9,8,7,6};
+int pinesColumnas[] = {5,4,3,2};
+char teclas[4][4] = {{'1','2','3','A'},
+                     {'4','5','6','B'},
+                     {'7','8','9','C'},
+                     {'*','0','#','D'}};
+int cantidadDeDatosIngresadosPorTeclado=0;
+char  bufferTeclado[10];
 
 
 
@@ -53,24 +68,49 @@ int main(void){
 
 	return 0;
 }
+/**/
 
-/*
 void estadosAdmin(char datoDelTeclado){
-	if(modoCombustible=='0'){
+	if(modoCombustible=='0'){//ok
 		modoCombustible=datoDelTeclado;
 	}
-	else if(modoCombustible!='0' && modoCarga=='0'){
+	else if(modoCombustible!='0' && modoCarga=='0'){//ok
 		modoCarga=datoDelTeclado;
 	}
-	else{
-		estadoDispenser=datoDelTeclado;
+	else if(modoCombustible!='0' && modoCarga=='1'){
+		modoIngresarCantidad=datoDelTeclado;
+	}
+	else if(modoCombustible!='0' && modoCarga!='0'  && datoDelTeclado!='1'){//si es dsitinto de 1 espero q sea un 2 o 3
+		estadoDispenser='1';
+	}
+	else if(modoIngresarCantidad!='0'){
+		if(datoDelTeclado=='#')
+		{
+			cantidadDeLitrosACargar=atoi(bufferTeclado[0]) ;
+			//obtenés la cantidad de litros o pesos
+			cantidadDeDatosIngresadosPorTeclado=0;
+			resetEstados();
+			resetBufferTeclado();
+		}
+		else{
+			bufferTeclado[cantidadDeDatosIngresadosPorTeclado]=teclaPresionada;
+			cantidadDeDatosIngresadosPorTeclado++;
+			estadoDispenser='2';
+		}
+
 	}
 }
 
 void resetEstados(void){
 	modoCombustible='0';//0-Espera orden, 1-Carga Nafta, 2-Carga Gasoil
 	modoCarga='0';//0-Espera orden, 1-Cant de litros, 2-Hasta que llene, 3-Modo abierto
+	modoIngresarCantidad='0';//1-cantidad de litros, //2-cantidad de plata
 	estadoDispenser='0';//1-llenando, 2-Esperando evento de llenado
+}
+void resetBufferTeclado(void){
+	for(int i=0; i<10;i++){
+		bufferTeclado[i]=' ';
+	}
 }
 
 //cantidad de combustible en litros
@@ -82,8 +122,59 @@ float calcularCosto(int cantDeCombustible){
 		return precioGasoil*cantDeCombustible;
 	}
 	return -1.0;
-}*/
+}
 
+
+/*############################################Teclado############################################*/
+void configurarPuertosTeclado(){
+	int i;
+	for(i=0 ; i<4; i++){
+		LPC_GPIO2->FIODIR|=(1<<pinesFilas[i]);//pines como output de las filas del teclado
+		LPC_GPIO2->FIOCLR|=(1<<pinesFilas[i]);//pines de las filas en alto del teclado//Tenía puesto un SET, hay que ver q pasa
+	}
+	//el resto de los pines están como input y pull up asi que las columnas del teclado están configuradas ya...
+}
+
+/*Detecta la tecla que fue presionada, la podemos llamar desde el main periódicamete o por interrupciones externas*/
+void loopTeclado(){
+	//Barrido por las filas
+	char teclaPresionada=' ';
+	for (int nL=0; nL<4; nL++)
+	{
+	   LPC_GPIO2->FIOCLR|=(1<<pinesFilas[nL]);
+	   //Barrido en columnas buscando un LOW
+	   for (int nC=0; nC<4; nC++) {
+		   if (! LPC_GPIO2->FIOPIN & (1<<pinesColumnas[nC]))//sie stán en low los pines
+		   {
+	          teclaPresionada=teclas[nL][nC];
+	          while(!LPC_GPIO2->FIOPIN & (1<<pinesColumnas[nC]))//(digitalRead(pinesColumnas[nC]) == LOW)
+	          {}
+	        }
+	   }
+	   LPC_GPIO2->FIOSET|=(1<<pinesFilas[nL]);
+	   }
+	   retardoEnMs(10);
+	   estadosAdmin(teclaPresionada);
+}
+
+/*condiguración para habilitar interrupciones por GPIO para el teclado matricial*/
+void confIntGPIOPorEINT(void){
+	for(int i=0; i<4;i++){
+		LPC_GPIOINT -> IO2IntEnF |= ((1 << pinesColumnas[i])); //Selecciono la interrupcion por flanco de bajada
+		LPC_GPIOINT -> IO2IntClr |= ((1 << pinesColumnas[i])); //Limpia la bandera
+	}
+	NVIC_SetPriority(EINT3_IRQn, 0); 	//Prioridad para esta interrupcion
+	NVIC_EnableIRQ(EINT3_IRQn); 		//Habilita las interrupciones por GPIO
+}
+
+void EINT3_IRQHandler(void){
+	NVIC_DisableIRQ(EINT3_IRQn);
+	for(int i=0; i<4;i++){
+		LPC_GPIOINT -> IO2IntClr |= ((1 << pinesColumnas[i])); //Limpia la bandera
+	}
+	loopTeclado();
+	NVIC_EnableIRQ(EINT3_IRQn);
+}
 
 
 
